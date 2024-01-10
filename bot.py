c577b6953ed7e3121c13ded06574d7123423d006
Marcel Kitzbichler <marcel.kitzbichler@gmail.com>
2024-01-10 17:07:28 +0100

    added some basic servo controller

--- bot.py
+++ bot.py
@@ -3,15 +3,17 @@ import time
 
 
 def forward():
-    mbot2.servo_set(87,"all")
-    mbot2.straight(10)
+    mbot2.servo_set(91,"all")
+    cyberpi.broadcast("handleServo")
+    mbot2.straight(10,10)
+    cyberpi.stop_other()
     mbot2.servo_set(95,"all")
 
 
 def back():
-    mbot2.turn(2)
+    mbot2.turn(3)
     mbot2.straight(-10)
-    mbot2.turn(-2)
+    mbot2.turn(-3)
 
 def drawLine():
     forward()
@@ -24,3 +26,11 @@ def main():
     time.sleep(1)
     while True:
         drawLine()
+
+@event.receive("handleServo")
+def servoHandler():
+    while True:
+        mbot2.servo_set(88.4,"all")
+        time.sleep(0.2)
+        mbot2.servo_set(95,"all")
+        time.sleep(0.2)
