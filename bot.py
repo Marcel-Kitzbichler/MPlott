7fec731271d900acb5a1424222f5268b8d053e7b
Marcel Kitzbichler <marcel.kitzbichler@gmail.com>
2024-01-10 20:53:00 +0100

    made timing depend on decoder

--- bot.py
+++ bot.py
@@ -2,31 +2,36 @@ import event, time, cyberpi, mbuild, mbot2
 import time
 
 imgBuffer =[
-    [True],
-    [False,True],
-    [False,False,True],
-    [False,False,False,True],
-    [False,False,False,False,True],
-    [False,False,False,False,False,True],
-    [False,False,False,False,False,False,True],
-    [False,False,False,False,False,False,False,True],
-    [False,False,False,False,False,False,False,False,True],
-    [False,False,False,False,False,False,False,False,False,True]
+    [False,True,False,False,False,False,False,True,False],
+    [False,True,False,False,False,False,False,True,False],
+    [False,True,True,False,False,False,True,True,False],
+    [False,True,True,False,False,False,True,True,False],
+    [False,True,False,True,False,True,False,True,False],
+    [False,True,False,True,False,True,False,True,False],
+    [False,True,False,False,True,False,False,True,False],
+    [False,True,False,False,True,False,False,True,False],
+    [False,True,False,False,False,False,False,True,False],
+    [False,True,False,False,False,False,False,True,False],
+    [False,True,False,False,False,False,False,True,False],
+    [False,True,False,False,False,False,False,True,False],
+    [False,True,False,False,False,False,False,True,False],
+    [False,True,False,False,False,False,False,True,False]
 ]
 currLine = []
 
 
 def forward():
+    mbot2.EM_reset_angle("EM1")
     mbot2.servo_set(95,"all")
     cyberpi.broadcast("handleServo")
-    mbot2.straight(10,10)
+    mbot2.straight(10,20)
     cyberpi.stop_other()
     mbot2.servo_set(95,"all")
 
 
 def back():
     mbot2.turn(3)
-    mbot2.straight(-10)
+    mbot2.straight(-10,20)
     mbot2.turn(-3)
 
 def drawLine():
@@ -40,16 +45,20 @@ def drwImg():
         drawLine()
 
 
-@event.start
+@event.is_press('a')
 def main():
+    time.sleep(2)
     drwImg()
 
 @event.receive("handleServo")
 def servoHandler():
-    for i in currLine:
+    while True:
+        pixel = int(mbot2.EM_get_angle("EM1")/17)
+        if pixel > len(currLine)-1:
+            mbot2.servo_set(95,"all")
+            cyberpi.stop_this()
+        i = currLine[pixel]
         if i:
             mbot2.servo_set(88.4,"all")
         else:
             mbot2.servo_set(95,"all")
-        time.sleep(0.2)
-    mbot2.servo_set(95,"all")
